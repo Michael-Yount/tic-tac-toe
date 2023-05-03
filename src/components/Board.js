80e97bf9b33295d61be34570c3d1de4003d39403
Michaeal Yount <cheshirekatsmile101@gmail.com>
2023-05-03 18:36:50 -0400

    showing mounted components

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,20 +4,23 @@ import Square from './Square';
 
 const Board = () => {
     const [player, setPlayer] = useState(1);
+    const [mounted, setMounted] = useState(true);
     let status = `Player ${player}`;
+    const toggle = () => setMounted(!mounted);
     function renderSquare(i) {
-        return <Square id={i}></Square>
+        return <Square id={i}player={player}></Square>
     }
     return (
     <> 
         <div className='container'>
             <div className='game-board'>
                 <div className='grid-row'>
-                {renderSquare(0)}
-                {renderSquare(1)}
-                {renderSquare(2)}
+                {mounted && renderSquare(0)}
+                {mounted && renderSquare(1)}
+                {mounted && renderSquare(2)}
                 </div>
                 <div id='info'>
+                    <button onClick={toggle}>Show/Hide Row</button>
                     <h1>{status}</h1>
                 </div>
             </div>
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Square = ({id}) => {
+const Square = ({id, player}) => {
+    //const [color, setColor] = useState();
+    //const palette = ['red', 'green', 'blue', 'yellow'];
+    //const getRandomColor = () =>
+    //    palette[Math.floor(Math.random() * 4)];
+   // keep track of the square 
     return (
-        <button className="btn">{id}</button>
+        //change color of square onClick
+        <button className="btn" onClick={() => {
+            alert(`I am square ${id}`)
+            //setColor(getRandomColor());
+            //e.target.style.background = color;
+        }}>{player}</button>
     )
 }
 
